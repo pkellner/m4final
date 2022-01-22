b4d01e6de269342b7fb32fa5d76c47140fe9ca4a
pkellner <peter@peterkellner.net>
2022-01-22 07:55:51 -0800

    icons

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,24 @@ class MyDocument extends Document {
           <link href="/style/bootstrap.min.css" rel="stylesheet" />
           <link href="/style/fontawesome/css/all.css" rel="stylesheet" />
           <link href="/style/style.css" rel="stylesheet" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
